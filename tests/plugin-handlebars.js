0b8aa390746699506023df627ffcce353713d6b9
Torsten Curdt <tcurdt@vafer.org>
2016-01-16 23:21:30 +0100

    test for partials

--- tests/plugin-handlebars.js
+++ tests/plugin-handlebars.js
@@ -5,23 +5,27 @@ const Test = require('blue-tape')
 const Type = require('../lib/enum').changes
 const _ = require('../lib/utils')
 
-function setup(t, options) {
+function setup(t) {
   const xs = require('../lib')
   const project = new xs('build', {
     url: 'URL',
     title: 'TITLE',
     author: 'JOHN DOE'
   })
-  const glob = project.glob
-
-  const pages = glob('content/**/*.txt')
-  const plugin = require('../lib/plugins/handlebars')(project)
-  return plugin(pages, options)
+  return project
 }
 
 Test('variable expansion from project, collection and document context', function(t) {
 
-  const collection = setup(t, { context:{ biffy: 'CLYRO' }})
+  const project = setup(t)
+  const glob = project.glob
+  const pages = glob('content/**/*.txt')
+  const plugin = require('../lib/plugins/handlebars')(project)
+
+  const options = {
+    context: { biffy: 'CLYRO' },
+  }
+  const collection = plugin(pages, options)
 
   return collection.update([
 
@@ -40,6 +44,47 @@ Test('variable expansion from project, collection and document context', functio
 
     return file.load.then(function(f){
       t.equal(f.body, 'page1:TITLE:FIGHTERS:CLYRO')
+    }).catch(function(err){ t.fail(err) })
+
+  })
+})
+
+Test('loading of partials', function(t) {
+
+  const project = setup(t)
+  const glob = project.glob
+  const pages = glob('content/**/*.txt')
+  const plugin = require('../lib/plugins/handlebars')(project)
+
+  const partials = glob('design/partials/**/*.txt')
+  const options = {
+    partials: partials,
+  }
+  const collection = plugin(pages, options)
+
+  return collection.update([
+
+    {
+      type: Type.A,
+      lmod: 1,
+      path: 'design/partials/base.txt',
+      load: _.lazyLoad({ body: 'PARTIAL', path: 'design/partials/base.txt' }),
+    },
+    {
+      type: Type.A,
+      lmod: 1,
+      path: 'content/page1.txt',
+      load: _.lazyLoad({ body: 'B:{{>base}}:E' }),
+    },
+
+  ]).then(function(changes1){
+
+    t.ok(collection.length === 1, 'has results')
+
+    const file = collection.get('content/page1.txt')
+
+    return file.load.then(function(f){
+      t.equal(f.body, 'B:PARTIAL:E')
     }).catch(function(err){ t.fail(err) })
 
   })
