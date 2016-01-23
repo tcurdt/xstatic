00905fb4776e8f5814436221c9bf69c646ab0d57
Torsten Curdt <tcurdt@vafer.org>
2016-01-23 15:01:13 +0100

    updated example

--- examples/json/build.js
+++ examples/json/build.js
@@ -1,16 +1,14 @@
 'use strict'
 
-const xs = require('../../lib')
-const project = new xs('build')
+const Xstatic = require('../../lib')
+const project = new Xstatic('build')
 
-const Babel = require('../../lib/plugins/babel')(project)
 const Merge = require('../../lib/plugins/merge')(project)
 const Filter = require('../../lib/plugins/filter')(project)
 const Sitemap = require('../../lib/plugins/sitemap')(project)
 const Template = require('../../lib/plugins/handlebars')(project)
 const Parser = require('../../lib/plugins/json')(project)
 const Glob = project.glob
-
 
 const layouts = Glob('design/layouts/*.html', { basedir: 'design/layouts' })
 const partials = Glob('design/partials/*.html', { basedir: 'design/partials' })
@@ -26,10 +24,6 @@ const itemsHtml = Template(itemsJson, {
 const assets = Glob('assets/**/*.+(js|jpg|png|ttf|css)', { basedir: 'assets' })
 const content = Merge([ itemsHtml ])
 const sitemap = Sitemap(content)
-
-// const html = Glob('html/**/*.html', { basedir: 'html' })
-// const javascript = Babel(Glob('app/**/*.+(js|jsx)'))
-// const bundles = Filter('**/*.js', [ javascript ])
 
 project.watch(Merge([ assets, sitemap, content ]))
 
--- examples/simple/build.js
+++ examples/simple/build.js
@@ -1,15 +1,18 @@
 'use strict'
 
 const Xstatic = require('../../lib')
-const project = new Xstatic('build')
+const project = new Xstatic('build', {
+  title: 'TITLE'
+})
 
 const Glob = project.glob
+const Frontmatter = require('../../lib/plugins/frontmatter')(project)
 const Template = require('../../lib/plugins/handlebars')(project)
 const Merge = require('../../lib/plugins/merge')(project)
 const Css = require('../../lib/plugins/css')(project)
 
 const css = Css(Glob('design/assets/**/*', { basedir: 'design/assets' }))
-const pages = Glob('pages/*.html', { basedir: 'pages' })
+const pages = Frontmatter(Glob('pages/*.html', { basedir: 'pages' }))
 const partials = Glob('design/partials/**/*.html', { basedir: 'design/partials' })
 
 const html = Template(pages, {
